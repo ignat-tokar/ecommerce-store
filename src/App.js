9ecb181751565f832ec9a8ae1dce5321afd3eb2f
Ignat Tokar <example@example.com>
2022-06-23 16:39:36 +0300

    added routing, realize empty components for pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Cart from './components/Cart';
+import CartOverlay from './components/CartOverlay';
+import Category from './components/Category';
+import ProductDisplayPage from './components/ProductDisplayPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="ecommerce-store/" element={<Category />}/>
+          <Route path="ecommerce-store/cart-overlay" element={<CartOverlay />}/>
+          <Route path="ecommerce-store/cart" element={<Cart />}/>
+          <Route path="ecommerce-store/product-display-page" element={<ProductDisplayPage />}/>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/CartOverlay.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class CartOverlay extends React.Component {
+  render() {
+    return (
+      <h1>CartOverlay</h1>
+    );
+  }
+}
+export default CartOverlay;
